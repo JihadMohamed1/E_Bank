972fa27b953d0640c29fb9d3f4ffca861b4fa545
jihad <totojihad2020@gmail.com>
2023-05-04 11:42:49 +0100

    version 0

---
+++ src/test/java/com/example/e_bank_backend/EBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_bank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
